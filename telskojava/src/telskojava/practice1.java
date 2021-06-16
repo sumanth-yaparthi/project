910e4e3646d7d3a0c4e74d4612bf540556906e17
YAPARTHI SUMANTH <sumanthyaparthi999@gmail.com>
2021-06-16 14:35:45 +0530

    first commit

---
+++ telskojava/src/telskojava/practice1.java
@@ -0,0 +1,49 @@
+package telskojava;
+
+public class practice1 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=4;i++)
+		{
+			if(i==1)
+			{
+				for(int j=1;j<=1;j++)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+		    }
+			if(i==2)
+			{
+				for(int j=1;j<=2;j++)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+			if(i==3)
+			{
+				for(int j=1;j<=3;j++)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+			if(i==4)
+			{
+				for(int j=1;j<=4;j++)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+			}
+		}
+	}
+}
+	
+			
+
+
+
+
---
+++ telskojava/src/telskojava/practice2.java
@@ -0,0 +1,43 @@
+package telskojava;
+
+public class practice2 
+{
+	public static void main(String[] args)
+	{
+		for(int i=4;i>=1;i--)
+		{
+			switch(i)
+			{
+			case 4:
+				for(int j=4;j>=1;j--)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+				break;
+			case 3:
+				for(int j=3;j>=1;j--)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+				break;
+			case 2:
+				for(int j=2;j>=1;j--)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+				break;
+			case 1:
+				for(int j=1;j>=1;j--)
+				{
+					System.out.print("* ");
+				}
+				System.out.println();
+				break;
+			}
+		}
+	}
+
+}
